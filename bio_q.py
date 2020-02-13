82e3b02fc6a7f28693d1efa38494a3926e148de1
Colton Avila <postlondon1666@gmail.com>
2020-02-13 14:44:50 -0600

    Switched from GET request to POST.
    Added the creation of a file with download links.

--- bio_q.py
+++ bio_q.py
@@ -1,4 +1,3 @@
-import os
 import sys
 sys.path.append('/usr/share/qgis/python/plugins')
 #QGIS Packages
@@ -6,10 +5,13 @@ from qgis.core import *
 from qgis.analysis import QgsNativeAlgorithms
 import processing
 from processing.core.Processing import Processing
-
 #GBIF
 from pygbif import occurrences as occ
 from pygbif import utils
+#POST Stuff
+import requests as r
+import time
+import json
 
 #INIT QGIS & PROCESSING W/ NATIVE TOOLS
 QgsApplication.setPrefixPath("/usr/bin/qgis",True)
@@ -19,7 +21,7 @@ Processing.initialize()
 QgsApplication.processingRegistry().addProvider(QgsNativeAlgorithms())
 
 #SHAPEFILE AS VECTOR
-pathSalado = "/home/utkimchi/Documents/RoM/Mexican_Basins_Project/Shapefiles/El_Salado.shp"
+pathSalado = "/home/utkimchi/Documents/RoM/Mexican_Basins_Project/Shapefiles/el_salado_smooth.shp"
 saladoL = QgsVectorLayer(pathSalado,"Salado","ogr")
 if not saladoL.isValid():
 	print("Layer Failed to Load")
@@ -42,7 +44,7 @@ for shape in fSalado.getFeatures():
 		sWKT = sGeo.asMultiPolygon()
 	tList = ""
 	fCoord = ""
-	#Checks the # of points that make up the polygon an sets them
+	#Checks the # of points that make up the polygon and sets them
 	#into the GBIF geomtry format
 	n = len(sWKT[0])
 	for xy in range(n):
@@ -57,13 +59,37 @@ for shape in fSalado.getFeatures():
 	polygonGeo[count] = 'POLYGON((' + tList + fCoord[:-2]+'))'
 	count += 1
 
+downloadLinks = {}
+
+for x in range(len(polygonGeo)):
+	print ("Looking at Polygon ", x)
+	print(polygonGeo[x])
+
+	#Proper WKT format
+	rwPG = utils.wkt_rewind(polygonGeo[x],digits=1)
+
+	url = 'http://api.gbif.org/v1/occurrence/download/request'
+
+	#JSON parameters
+	jsonPayload = {"creator":YOURNAME_STR, \
+	"notificationAddresses":[YOUREMAIL_STR], \
+	"format":"SIMPLE_CSV", \
+	'predicate':{'type':'within', 'geometry':rwPG}}
+
+	#POST Headers
+	headers = {'Content-Type': 'application/json'}
+
+	#Final POST request
+	req = r.post(url,headers=headers,json=jsonPayload,auth=(USERNAME_STR,PASSWORD_STR))
+	
+	#Generates download links for each polygon
+	print("Waiting 60 Seconds for download to complete")
+	time.sleep(60)
+	downloadreq = r.get('http://api.gbif.org/v1/occurrence/download/' + req.text)
+	ddic = json.loads(downloadreq.text)
+	downloadLinks[x] = ddic["downloadLink"]
+
+with open('dFile.txt','w') as f:
+	print(downloadLinks,file=f)
+
 qgs.exitQgis()
-
-print(polygonGeo[0])
-rwPG = utils.wkt_rewind(polygonGeo[0],digits=1)
-
-#ADD GBIF QUERY 
-
-ts = occ.search(geometry = rwPG, limit=2)
-
-print (ts)
